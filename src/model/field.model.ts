5b832867ec0e8c06f1288e085a5a2b7c3fb94f85
baniaf9897 <fabian9897@gmail.com>
2018-09-23 12:27:28 +0200

    adding field model

---
+++ src/model/field.model.ts
@@ -0,0 +1,14 @@
+export class Field {
+    isMine:boolean
+    value:any
+    isRevealed:boolean
+    x:number
+    y:number
+    constructor(isMine:boolean, xCoordinate:number, yCoordinate:number ){
+        this.isMine = isMine
+        this.value = 0
+        this.x = xCoordinate
+        this.y = yCoordinate
+        this.isRevealed = false
+    }
+}
